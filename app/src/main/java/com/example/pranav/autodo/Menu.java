16e5de5ed9bc9bd073d2e0a58476c9a344ec638d
cecautodo <34531957+cecautodo@users.noreply.github.com>
2017-12-16 11:16:49 +0530

    MEnu half

--- app/src/main/java/com/example/pranav/autodo/Menu.java
+++ app/src/main/java/com/example/pranav/autodo/Menu.java
@@ -2,8 +2,12 @@ package com.example.pranav.autodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+    Button rem;
+    Button
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
