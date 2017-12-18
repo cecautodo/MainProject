7b0e82ea4f85a69f4be3588d57c31eae36e13e05
cecautodo <34531957+cecautodo@users.noreply.github.com>
2017-12-18 11:51:20 +0530

    parental_home

--- app/src/main/java/com/example/pranav/autodo/Menu.java
+++ app/src/main/java/com/example/pranav/autodo/Menu.java
@@ -1,11 +1,16 @@
 package com.example.pranav.autodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
     Button rem;
+    Button profile;
+    Button calllist;
+    Button parental;
 
 
 
@@ -13,5 +18,17 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        rem=(Button) findViewById(R.id.Reminder);
+        profile=(Button) findViewById(R.id.Profile);
+        calllist=(Button)findViewById(R.id.calllist);
+        parental=(Button)findViewById(R.id.Parental);
+        parental.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent n=new Intent(getApplicationContext(),parental_home.class);
+                startActivity(n);
+            }
+        });
+
     }
 }
