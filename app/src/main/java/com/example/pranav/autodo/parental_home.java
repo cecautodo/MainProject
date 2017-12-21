f0886d97171c3aac2e3e45073bef91e3e5023ce4
cecautodo <34531957+cecautodo@users.noreply.github.com>
2017-12-21 14:51:59 +0530

    authentication

--- app/src/main/java/com/example/pranav/autodo/parental_home.java
+++ app/src/main/java/com/example/pranav/autodo/parental_home.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class parental_home extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
